9a77db2df44cc67a480aa0dd54ed5e23ea70bf0a
Valentin Grishin <kolumbus59@gmail.com>
2023-12-01 19:30:14 +0300

    Add brain-even.js and game logic to src/game/even.js, updated bin section in package.json. Recorded asciinema and added link to README.md.

---
+++ src/game/even.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (num) => num % 2 === 0;
+
+const playBrainEven = () => {
+  const countToWin = 3;
+  let correctAnswerCount = 0;
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  while (correctAnswerCount < countToWin) {
+    const randomNumber = Math.floor(Math.random() * 100 + 1);
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer.toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+      correctAnswerCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+export default playBrainEven;
